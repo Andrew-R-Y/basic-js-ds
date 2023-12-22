fb327ebfe0baa8542241ba056222e548e4043eb0
Andrew-R-Y <rakitinandrei@gmail.com>
2023-12-23 00:08:58 +0300

    feat: add partial solutions of BinarySearchTree task. Implement root, add, min, max methods

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,21 +1,53 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
 // const { Node } = require('../extensions/list-tree.js');
-
 /**
-* Implement simple binary search tree according to task description
-* using Node from extensions
-*/
+ * Implement simple binary search tree according to task description
+ * using Node from extensions
+ */
 class BinarySearchTree {
-
-  root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.rootEl = null;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  root() {
+    return this.rootEl;
+  }
+
+  add(data) {
+    function makeNode(value) {
+      let result = {};
+      result.data = value;
+      result.left = null;
+      result.right = null;
+      return result;
+    }
+    const root = this.rootEl;
+    if (!root) {
+      this.rootEl = makeNode(data);
+    } else {
+      let current = root;
+      function addItem(current) {
+        if (current.data === data) {
+          return;
+        }
+        if (data < current.data) {
+          if (!current.left) {
+            current.left = makeNode(data);
+          } else {
+            addItem(current.left);
+          }
+        } else {
+          if (!current.right) {
+            current.right = makeNode(data);
+          } else {
+            addItem(current.right);
+          }
+        }
+      }
+
+      addItem(current);
+    }
   }
 
   has(/* data */) {
@@ -34,16 +66,30 @@ class BinarySearchTree {
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.rootEl) {
+      return false;
+    }
+    let current = this.rootEl;
+    while (current.left) {
+      current = current.left;
+    }
+    let result = current.data;
+    return result;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.rootEl) {
+      return false;
+    }
+    let current = this.rootEl;
+    while (current.right) {
+      current = current.right;
+    }
+    let result = current.data;
+    return result;
   }
 }
 
 module.exports = {
-  BinarySearchTree
+  BinarySearchTree,
 };
